82542c1a7a496577808824f8287a202b6d342651
Maciej Handke <maciejhandke11@gmail.com>
2022-05-08 21:19:45 +0200

    Add resolution and ship's speed

---
+++ settings.py
@@ -0,0 +1,7 @@
+class Settings:
+    def __init__(self):
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (38, 38, 38)
+        self.ship_speed_factor = 0.85
+
