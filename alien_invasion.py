29b9f9c88998f3c3f8a78edd2e13148da74d9885
Maciej Handke <maciejhandke11@gmail.com>
2022-05-14 10:41:28 +0200

    Add increasing game speed in next rounds

--- alien_invasion.py
+++ alien_invasion.py
@@ -15,7 +15,7 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
     bullets = Group()
     aliens = Group()
-    play_button = Button(ai_settings, screen, "Gra")
+    play_button = Button(ai_settings, screen, "Play")
     stats = GameStats(ai_settings)
     gf.create_fleet(ai_settings, screen, ship, aliens)
 
--- settings.py
+++ settings.py
@@ -1,15 +1,29 @@
 class Settings:
     def __init__(self):
+        # Screen
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (38, 38, 38)
-        self.ship_speed_factor = 0.85
-        self.bullet_speed_factor = 0.5
+        # Ship
+        self.ship_limit = 3
+        # Bullet
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = 0, 255, 68
         self.bullets_allowed = 3
-        self.alien_speed_factor = 0.1
+        # Alien
         self.fleet_drop_speed = 10
+        # Game speed
+        self.speedup_scale = 1.1
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        self.ship_speed_factor = 0.85
+        self.bullet_speed_factor = 0.5
+        self.alien_speed_factor = 0.1
         self.fleet_direction = 1
-        self.ship_limit = 3
+
+    def increase_speed(self):
+        self.ship_speed_factor *= self.speedup_scale * 0.7
+        self.bullet_speed_factor *= self.speedup_scale
+        self.alien_speed_factor *= self.speedup_scale
