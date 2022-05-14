ddde8b1e1079eab67408c7bf13da3f153d78700e
Maciej Handke <maciejhandke11@gmail.com>
2022-05-14 12:07:06 +0200

    Add game scoring and player's ships left

--- settings.py
+++ settings.py
@@ -7,7 +7,7 @@ class Settings:
         # Ship
         self.ship_limit = 3
         # Bullet
-        self.bullet_width = 3
+        self.bullet_width = 30000
         self.bullet_height = 15
         self.bullet_color = 0, 255, 68
         self.bullets_allowed = 3
@@ -22,8 +22,9 @@ class Settings:
         self.bullet_speed_factor = 0.5
         self.alien_speed_factor = 0.1
         self.fleet_direction = 1
+        self.alien_points = 1
 
     def increase_speed(self):
-        self.ship_speed_factor *= self.speedup_scale * 0.7
+        self.ship_speed_factor *= self.speedup_scale
         self.bullet_speed_factor *= self.speedup_scale
         self.alien_speed_factor *= self.speedup_scale
